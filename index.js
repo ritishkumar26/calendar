85b36a14dd48cb4a1e3a7c0c9d47581ff66992a5
nobita <89516405+ritishkumar26@users.noreply.github.com>
2022-12-24 00:05:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,44 +2,53 @@ const months = ["January", "February", "March", "April", "May", "June", "July", 
 const days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
 const Yrange =[31,28,31,30,31,30,31,31,30,31,30,31];
 var currentyr = parseInt(document.getElementById("currentyear").innerText)
-function showmth(range)
+const day =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31];
+var cnt =parseInt(document.getElementById("count").innerText);
+function showmth(range,currentyr,cnt)
 {  
+ var d = new Date(currentyr,cnt,1);
+ var getday = d.getDay(); 
+ var space ="";
+ for(i=0;i<getday;i++)
+ {
+    day.unshift("");
+ }
+
  var content2="";
-    var num=1;
-    content2 = `<table class="table2">`;
+ var k=0;
     for(i=1;i<=days.length;i++)
     {
         content2+=`<tr>`;
         for(j=1;j<=days.length;j++)
         {
-            if(num<=range)
+            if(k<range+getday)
             {
-                content2+=`<td>${num}</td>`;
-                num= num+1;   
+                content2+=`<div class="days">${day[k]}</div>`;
+                 k=k+1; 
             }       
         }
-         content2+=`</tr>`;   
     }
-    content2+= `</table>`;
     document.getElementById("table2").innerHTML =content2;
+    for(i=0;i<getday;i++)
+    {
+       day.shift("");
+    }
 }
  var content="";
-    content += `<table class="table1"><tr>`;
     for(j=0;j<days.length;j++)
     {
-        content+=`<td>${days[j]}</td>`;
+        content+=`<div class="days">${days[j]}</div>`;
     }
-content+= `</tr></table>`;
 document.getElementById("table1").innerHTML =content;
 
-showmth(Yrange[0]);
+showmth(Yrange[0],currentyr,cnt);
 document.querySelectorAll("button")[1].addEventListener('click',()=>{
 var currentyr = parseInt(document.getElementById("currentyear").innerText)
 var currmnth = document.getElementById("currentmonth").innerText;
 var cnt =parseInt(document.getElementById("count").innerText);
 if(currmnth=="December"&&cnt==11)
     {
-    showmth(Yrange[cnt]);
+    showmth(Yrange[cnt],currentyr,cnt);
     cnt=0;
     currentyr= currentyr+1;
     document.getElementById("currentyear").innerText =currentyr;
@@ -52,7 +61,7 @@ if(currmnth=="December"&&cnt==11)
         document.getElementById("currentmonth").innerText = months[cnt];
         document.getElementById("count").innerText =cnt;
  Yrange[1]=((currentyr%4==0)&&(currentyr%100!=0)||(currentyr%400==0))?29:28;
-        showmth(Yrange[cnt]);
+        showmth(Yrange[cnt],currentyr,cnt);
     }
 })
 document.querySelectorAll("button")[0].addEventListener('click',()=>{
@@ -61,7 +70,7 @@ document.querySelectorAll("button")[0].addEventListener('click',()=>{
     var cnt =parseInt(document.getElementById("count").innerText);
         if(currmnth=="January"&&cnt==0)
         {
-       showmth(Yrange[cnt]);
+       showmth(Yrange[cnt],currentyr,cnt);
         cnt=11;
         currentyr= currentyr-1;
         document.getElementById("currentyear").innerText =currentyr;
@@ -74,7 +83,7 @@ document.querySelectorAll("button")[0].addEventListener('click',()=>{
             document.getElementById("currentmonth").innerText = months[cnt];
             document.getElementById("count").innerText =cnt;
             Yrange[1]=((currentyr%4==0)&&(currentyr%100!=0)||(currentyr%400==0))?29:28;
-         showmth(Yrange[cnt]);
+           showmth(Yrange[cnt],currentyr,cnt);
         }
     })
     
